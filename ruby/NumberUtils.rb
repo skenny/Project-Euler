6d71e39f6a4df0c28473bab09bd6368bcb6de46e
U-SDI\skenny <skenny@Thaddeus.SDI>
2010-07-20 13:16:56 -0300

    Added

---
+++ ruby/NumberUtils.rb
@@ -0,0 +1,48 @@
+#!/usr/env/ruby
+
+require 'mathn'
+
+class NumberUtils
+
+    def isPrime(num)
+        if num == 2
+            return true
+        end
+        
+        if num % 2 == 0
+            return false
+        end
+        
+        (3..Math.sqrt(num)).step 2 do |i|
+            if num % i == 0
+                return false
+            end
+        end
+        
+        return true
+    end
+
+    def primeFactors(num)
+        primeFactors = Array.new
+        
+        (1..num).each do |i|
+            if isPrime(i) and num % i == 0
+                primeFactors << i
+            end
+        end
+        
+        return primeFactors
+    end
+
+    if __FILE__ == $0
+        nu = NumberUtils.new
+        
+        puts "7: #{nu.isPrime(7)}"
+        puts "8: #{nu.isPrime(8)}"
+        puts "53: #{nu.isPrime(53)}"
+        
+        puts "741273: #{nu.primeFactors(741273)}"
+    end
+
+end
+
